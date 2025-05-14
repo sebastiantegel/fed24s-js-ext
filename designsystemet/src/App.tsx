b892181837c2676a04a20a1ec653b4a4f43c1ed6
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-05-14 10:19:19 +0200

    Updated with designsystemet

--- designsystemet/src/App.tsx
+++ designsystemet/src/App.tsx
@@ -1,7 +1,83 @@
+import {
+  DigiBarChart,
+  DigiButton,
+  DigiLayoutBlock,
+  DigiLayoutColumns,
+  DigiTypography,
+} from "@digi/arbetsformedlingen-react";
 import "./App.css";
+import {
+  BarChartVariation,
+  LayoutBlockContainer,
+  LayoutBlockVariation,
+  LayoutColumnsElement,
+  LayoutColumnsVariation,
+  TypographyVariation,
+} from "@digi/arbetsformedlingen";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <DigiLayoutBlock
+        afContainer={LayoutBlockContainer.STATIC}
+        afVariation={LayoutBlockVariation.PRIMARY}
+      >
+        <DigiTypography afVariation={TypographyVariation.SMALL}>
+          <DigiLayoutColumns
+            afElement={LayoutColumnsElement.DIV}
+            afVariation={LayoutColumnsVariation.THREE}
+          >
+            <div>
+              <DigiButton
+                afFullWidth={false}
+                afSize="large"
+                afVariation="primary"
+                onAfOnClick={() => {
+                  console.log("Klick");
+                }}
+              >
+                Spara
+              </DigiButton>
+            </div>
+            <div>
+              <DigiButton
+                afFullWidth={false}
+                afSize="large"
+                afVariation="secondary"
+                onAfOnClick={() => {
+                  console.log("Cancel");
+                }}
+              >
+                Avbryt
+              </DigiButton>
+            </div>
+            <div>
+              <div style={{ width: "400px", height: "400px" }}>
+                <DigiBarChart
+                  afVariation={BarChartVariation.Vertical}
+                  afChartData={{
+                    data: {
+                      xValues: [10, 50],
+                      xValueNames: ["Antal 1", "Antal 2"],
+                      series: [
+                        {
+                          yValues: [20, 40],
+                          title: "Antal studenter",
+                        },
+                      ],
+                    },
+                    x: "Studenter",
+                    y: "Antal",
+                    title: "My awesome chart",
+                  }}
+                ></DigiBarChart>
+              </div>
+            </div>
+          </DigiLayoutColumns>
+        </DigiTypography>
+      </DigiLayoutBlock>
+    </>
+  );
 }
 
 export default App;
